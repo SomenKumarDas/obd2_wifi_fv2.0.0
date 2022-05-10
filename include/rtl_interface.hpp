b5e0b3ddac28ff78554ed0d8b56f4ec3c7cfbd93
SomenKumarDas <somen.k.das2@gmail.com>
2022-05-10 15:28:50 +0530

    rtl com uart2

--- include/rtl_interface.hpp
+++ include/rtl_interface.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
 
-#define RTL_DATA_CH Serial1
+#define RTL_DATA_CH Serial2
 
 void rtl_interface_Task(void *pv);
 void rtl_interface_write(uint8_t *payLoad, uint16_t len);
